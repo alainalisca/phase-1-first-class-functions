6856bd3d4d8e880a17a5b4c8bea13aeb8864e5b4
Alain Alisca <alainalisca@gmail.com>
2022-04-10 22:28:34 -0400

    commit tree 45012301f7460db1035d5140d05b1c0fb0615583

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction (tenMileRun){
+    tenMileRun();
+} 
+
+function returnsANamedFunction(){
+return function AlexisRoseGervasini(){}
+}
+
+ function returnsAnAnonymousFunction(){ 
+return function (){}
+ }
